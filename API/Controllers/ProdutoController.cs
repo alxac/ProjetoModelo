0acccd9929f57a1ea62c1598ed2119b942e19415
alxac <alxac@msn.com>
2019-11-19 14:53:52 -0400

    Criação do Projeto API

---
+++ API/Controllers/ProdutoController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Aplicacao.Interface;
+using Domain.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/produto")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoApp produtoApp;
+
+        public ProdutoController(IProdutoApp _produtoApp)
+        {
+            produtoApp = _produtoApp;
+        }
+        // GET: api/Produto
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            return produtoApp.Listar();
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public Produto Get(int id)
+        {
+            return produtoApp.ObterPorId(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Produto value)
+        {
+            produtoApp.Adicionar(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Produto value)
+        {
+            produtoApp.Atualizar(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            produtoApp.Excluir(produtoApp.ObterPorId(id));
+        }
+    }
+}
